db1dc58fb0c19033d96cc2b8f96e588eabe3c7bb
kmollencopf <km@kmdeveloping.com>
2025-04-14 11:31:33 -0400

    fix config

--- HybridCache/Configuration/HybridCacheOptions.cs
+++ HybridCache/Configuration/HybridCacheOptions.cs
@@ -2,10 +2,16 @@ namespace HybridCache.Configuration;
 
 public class HybridCacheOptions
 {
-    public bool RedisDistributedCacheEnabled { get; set; }
     public TimeSpan? MemoryCacheDuration { get; set; }
     public TimeSpan? DistributedCacheDuration { get; set; }
-    public TimeSpan? DefaultSlidingExpiration { get; set; }
+    public TimeSpan? DefaultDistributedSlidingExpiration { get; set; }
+    public TimeSpan? DefaultMemorySlidingExpiration { get; set; }
+    public RedisCacheOptions RedisCacheOptions { get; set; } = null!;
+}
+
+public class RedisCacheOptions
+{
+    public bool RedisDistributedCacheEnabled { get; set; }
     public string? RedisDistributedCacheConnectionString { get; set; }
     public string? RedisDistributedCacheInstanceName { get; set; } 
 }
