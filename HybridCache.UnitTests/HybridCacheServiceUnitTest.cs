0eea706499305976364f66a44255380e2809586f
kmollencopf <km@kmdeveloping.com>
2025-04-16 11:05:57 -0400

    add unit tests

---
+++ HybridCache.UnitTests/HybridCacheServiceUnitTest.cs
@@ -0,0 +1,15 @@
+﻿namespace HybridCache.UnitTests;
+
+public class HybridCacheServiceUnitTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
