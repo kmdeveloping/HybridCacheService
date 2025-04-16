e7338add9c94d09fa44ec70b7ca568eb8a508708
kmollencopf <km@kmdeveloping.com>
2025-04-15 21:08:08 -0400

    wip

--- HybridCache/HybridCacheService.cs
+++ HybridCache/HybridCacheService.cs
@@ -5,6 +5,12 @@ using Microsoft.Extensions.Caching.Distributed;
 
 namespace HybridCache;
 
+public interface IHybridCacheService
+{
+    Task<T?> GetOrSetAsync<T>(string key, Func<Task<T>> service) where T : class;
+    T? GetOrSet<T>(string key, Func<T> service) where T : class;
+}
+
 public class HybridCacheService(HybridCacheOptions options, IMemoryCache memoryCache, IDistributedCache distributedCache) : IHybridCacheService
 {
     private readonly IMemoryCache _memoryCache = memoryCache ?? throw new ArgumentNullException(nameof(memoryCache));
--- HybridCache/IHybridCacheService.cs
+++
@@ -1,7 +0,0 @@
-namespace HybridCache;
-
-public interface IHybridCacheService
-{
-    Task<T?> GetOrSetAsync<T>(string key, Func<Task<T>> service) where T : class;
-    T? GetOrSet<T>(string key, Func<T> service) where T : class;
-}
